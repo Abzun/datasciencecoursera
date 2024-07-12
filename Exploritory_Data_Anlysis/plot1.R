fe8fc6b1376bef941bdbaca240b41fbd2db9e23a
Abzun <Medrano972@gmail.com>
2024-07-12 15:59:50 -0400

    Update plot1.R

--- Exploritory_Data_Anlysis/plot1.R
+++ Exploritory_Data_Anlysis/plot1.R
@@ -1,13 +1,13 @@
-# Load data
+# Loaded data from my pc. 
 data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings="?", stringsAsFactors=FALSE)
 data$Date <- as.Date(data$Date, format="%d/%m/%Y")
 subset_data <- subset(data, Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"))
 
-# Convert to Date/Time classes
+# Convert to the datetime classes
 datetime <- strptime(paste(subset_data$Date, subset_data$Time), format="%Y-%m-%d %H:%M:%S")
 subset_data$DateTime <- datetime
 
-# Plot 1: Global Active Power
+# First Plot Global Active Power
 png("plot1.png", width=480, height=480)
 plot(subset_data$DateTime, subset_data$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
