bb42a732e35b20b106410ee7626285ee3ec30d66
Abzun <Medrano972@gmail.com>
2024-07-12 16:02:28 -0400

    Update plot4.R

--- Exploritory_Data_Anlysis/plot4.R
+++ Exploritory_Data_Anlysis/plot4.R
@@ -6,6 +6,7 @@ subset_data <- subset(data, Date >= as.Date("2007-02-01") & Date <= as.Date("200
 # Convert to Date/Time classes
 datetime <- strptime(paste(subset_data$Date, subset_data$Time), format="%Y-%m-%d %H:%M:%S")
 subset_data$DateTime <- datetime
+#----^^^^ THIS IS THE SAME AS THE OTHER PLOTS^^^^ ----
 
 # Plot 4: Multiple Plots
 png("plot4.png", width=480, height=480)
