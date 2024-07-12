967ff798fae0f3ca71b23498a96e4620da332220
Abzun <Medrano972@gmail.com>
2024-07-12 15:56:39 -0400

    Plot3

---
+++ Exploritory_Data_Anlysis/plot3.R
@@ -0,0 +1,16 @@
+# Load data
+data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings="?", stringsAsFactors=FALSE)
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+subset_data <- subset(data, Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"))
+
+# Convert to Date/Time classes
+datetime <- strptime(paste(subset_data$Date, subset_data$Time), format="%Y-%m-%d %H:%M:%S")
+subset_data$DateTime <- datetime
+
+# Plot 3: Energy Sub Metering
+png("plot3.png", width=480, height=480)
+plot(subset_data$DateTime, subset_data$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+lines(subset_data$DateTime, subset_data$Sub_metering_2, col="red")
+lines(subset_data$DateTime, subset_data$Sub_metering_3, col="blue")
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lty=1)
+dev.off()
